304af8697be7c9d69a65e5a42ae10980020c41e9
Chris <cpaterson152@gmail.com>
2023-08-03 19:58:27 -0700

    Refactor JavaScript

--- quiz/script.js
+++ quiz/script.js
@@ -1,37 +1,30 @@
 const form = document.querySelector("#quiz-form");
-const inputs = document.querySelectorAll(".answer");
+const answers = document.querySelectorAll(".answer");
 const questions = document.querySelectorAll(".question-item");
 const congrats = document.querySelector("#alert");
 const NUMBER_OF_QUESTIONS = questions.length;
 
 form.addEventListener("submit", e => {
   e.preventDefault();
-  const inputArray = Array.from(inputs);
-  let selectedInputs = inputArray.filter(input => input.checked === true);
-  let points = 0;
+  let selectedAnswers = Array.from(answers).filter(a => a.checked);
+  let correctResponses = 0;
 
   questions.forEach(question => {
-    let questionInputsArray = Array.from(question.querySelectorAll("input"));
-    let isUnanswered = questionInputsArray.every(
-      input => input.checked === false
-    );
-    if (isUnanswered) {
-      markIncorrect(isUnanswered);
-    }
+    let questionInputs = Array.from(question.querySelectorAll("input"));
+    let isUnanswered = questionInputs.every(a => !a.checked);
+    if (isUnanswered) markIncorrect(question);
   });
 
-  selectedInputs.forEach(answer => {
-    points++;
+  selectedAnswers.forEach(answer => {
     if (answer.value === "true") {
-      let isCorrect = answer.closest(".question-item");
-      markCorrect(isCorrect);
-    } else if (answer.value === "false") {
-      let notCorrect = answer.closest(".question-item");
-      markIncorrect(notCorrect);
+      correctResponses++;
+      markCorrect(answer.closest(".question-item"));
+    } else {
+      markIncorrect(answer.closest(".question-item"));
     }
   });
 
-  if (points === NUMBER_OF_QUESTIONS) {
+  if (correctResponses === NUMBER_OF_QUESTIONS) {
     congrats.style.display = "block";
   }
 });
